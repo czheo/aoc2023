2db4ca49465354db854a3bfd7b703fb219d5e0b0
czheo <1692400+czheo@users.noreply.github.com>
2023-12-01 22:31:30 -0800

    day 2 part 1

---
+++ day_02/part1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int id;
+  int ans = 0;
+  while (scanf("Game %d:", &id) != EOF) {
+    while (1) {
+      int red = 0, green = 0, blue = 0;
+      int n;
+      char color[10];
+      char sep;
+      scanf("%d %[a-z]", &n, color);
+      scanf("%c", &sep);
+      if (strcmp(color, "red") == 0) {
+        red = n;
+      } else if (strcmp(color, "green") == 0) {
+        green = n;
+      } else if (strcmp(color, "blue") == 0) {
+        blue = n;
+      }
+      if (red > 12 || green > 13 || blue > 14) {
+        while (sep != '\n' && getchar() != '\n');
+        break;
+      } else if (sep == '\n') {
+        ans += id;
+        break;
+      }
+    }
+  }
+  printf("%d\n", ans);
+}
