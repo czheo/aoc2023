bc0579b8cd4f4427af1869504c1a5d3635c0fccd
czheo <1692400+czheo@users.noreply.github.com>
2023-12-01 22:35:24 -0800

    day 2 part 2

--- day_02/part2.cpp
+++ day_02/part2.cpp
@@ -4,8 +4,28 @@ using namespace std;
 
 int main()
 {
-  string line;
-  while (getline(cin, line)) {
-    // do something with the line
+  int id;
+  int ans = 0;
+  while (scanf("Game %d:", &id) != EOF) {
+    int red = 0, green = 0, blue = 0;
+    while (1) {
+      int n;
+      char color[10];
+      char sep;
+      scanf("%d %[a-z]", &n, color);
+      scanf("%c", &sep);
+      if (strcmp(color, "red") == 0) {
+        red = max(red, n);
+      } else if (strcmp(color, "green") == 0) {
+        green = max(green, n);
+      } else if (strcmp(color, "blue") == 0) {
+        blue = max(blue, n);
+      }
+      if (sep == '\n') {
+        ans += red * green * blue;
+        break;
+      }
+    }
   }
+  printf("%d\n", ans);
 }
